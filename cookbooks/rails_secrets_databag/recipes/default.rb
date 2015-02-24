358563e6f85a042cb1cb9745e3edf09fec246c6c
Clint Bishop <clintonbishop@gmail.com>
2015-02-24 11:48:03 -0600

    Encrypted databag

--- cookbooks/rails_secrets_databag/recipes/default.rb
+++ cookbooks/rails_secrets_databag/recipes/default.rb
@@ -3,15 +3,22 @@
 # Recipe:: default
 #
 
+# Keep the encrypted_data_bag_secret in the snapshot so it can be
+# copied over when a new instance boots. This way it only has to
+# be done once per environment, instead of on every boot.
 if ['app_master', 'app', 'util', 'solo'].include?(node[:instance_role])
   node[:applications].each do |app, data|
+    l = link "/etc/chef/encrypted_data_bag_secret" do
+      to "/data/encrypted_data_bag_secret"
+    end
+
     template "/data/#{app}/shared/config/secrets.yml"do
       source 'secrets.yml.erb'
       owner node[:owner_name]
       group node[:owner_name]
       mode 0600
       backup 0
-      variables(secrets: Chef::EncryptedDataBagItem.load("secrets", app)["contents"])
+      variables(app: app)
     end
   end
 end
